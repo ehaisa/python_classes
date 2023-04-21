f8fb7c42d4eba5c6a8c45ecd6006af22fa75e72f
ehaisa <isalaura1304@gmail.com>
2023-04-21 16:11:11 -0300

    first commit

--- semana3/variaveis2.py
+++ semana3/variaveis2.py
@@ -1,9 +1,13 @@
+# Segundo modo, visto em aula
+
 a = int(input("Informe um valor para A: "))
 b = int(input("Informe um valor para B: "))
 print("Invertendo os valores...\n")
-a = a + b
-b = a - b
-a = a - b
+
+c = a
+a = b
+b = c
+
 print("Novo valor de A:", a)
 print("Novo valor de B:", b)
 print("Fim do Programa")
---
+++ semana3/variaveis3.py
@@ -0,0 +1,8 @@
+# Terceiro modo, visto no youtube
+
+a = int(input("Defina um valor para A: "))
+b = int(input("Defina um valor para B: "))
+a, b = b, a
+
+print("Novo valor de A: ", a)
+print("Novo valor de B: ", b)
