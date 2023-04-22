36e8ef99da550888472ce73de3584658c09aec1a
ehaisa <isalaura1304@gmail.com>
2023-04-22 14:15:41 -0300

    estudando variaveis

--- semana3/vtarefa5.py
+++ semana3/vtarefa5.py
@@ -1,5 +1,6 @@
 print("Geometricamente falando")
 print("calculadora para trapézio, losango e circulo")
+print()
 
 print("CALCULO DO TRAPÉZIO")
 
@@ -9,7 +10,7 @@ altura = float(input("Defina um valor para a altura: "))
 
 areaT = (bMaior + bMenor) * altura / 2
 print("A area do trapézio é %.2f"% areaT)
-
+print()
 
 print("CALCULO DO LOSANGO")
 
@@ -18,7 +19,7 @@ dMenor = float(input("Defina um valor para a diagonal menor: "))
 
 areaL = (dMaior * dMenor) / 2
 print("A area do losango é %.2f"% areaL)
-
+print()
 
 print("CALCULO DO CIRCULO")
 
