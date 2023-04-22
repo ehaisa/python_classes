58510679f1bd3b95cd570e8e83d16cd92ddfb042
ehaisa <isalaura1304@gmail.com>
2023-04-21 22:20:13 -0300

    initial commit

---
+++ variaveis4.py
@@ -0,0 +1,7 @@
+largura = float(input("Valor da largura em cm: "))
+comprimento = float(input("Valor do comprimento em cm: "))
+comprimento = (comprimento / 100)
+largura = (largura / 100)
+area= comprimento * largura
+
+print("O valor da area é: %.1f m²"% area)
