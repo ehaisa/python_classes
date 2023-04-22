8d03f23ab85ca98ab9627119298bed93ba3e7f45
ehaisa <isalaura1304@gmail.com>
2023-04-22 13:42:08 -0300

    estudando variaveis

--- semana3/vtarefa1.py
+++ semana3/vtarefa1.py
@@ -1,7 +1,9 @@
 largura = float(input("Valor da largura em cm: "))
 comprimento = float(input("Valor do comprimento em cm: "))
+
 comprimento = (comprimento / 100)
 largura = (largura / 100)
+
 area= comprimento * largura
 
 print("O valor da area é: %.1f m²"% area)
