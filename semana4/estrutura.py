56168b51e68458070d5953870c203233fbfa324c
ehaisa <isalaura1304@gmail.com>
2023-04-22 17:44:49 -0300

    estudando estruturas

--- semana4/estrutura.py
+++ semana4/estrutura.py
@@ -4,7 +4,7 @@ data = datetime.date.today()
 idade_limite = 60
 
 nome = input("Olá! Qual o seu nome? ")
-print("Seja bem-vindo.")
+print("Seja bem-vindo(a).")
 
 ano_nasc = int(input("Em que ano você nasceu?"))
 ano_atual = data.year
@@ -14,5 +14,7 @@ if idade >= idade_limite:
     print("%s, este ano você completa %d anos! Já se vacinou contra a COVID?"% (nome, idade))
     print("Estou vendo aqui que já chegou na sua faixa etária")
     print("Se não tomou ainda, procure um posto de saúde")
-
+else:
+    print("%s, este ano você completa %d anos! Infelizmente ainda não chegou a sua vez para se vacinar."%(nome, idade))
+    print("Por favor, fique atento(a) ao calendário de vacinação.")
 print("Esperamos que tenha um bom dia!")
