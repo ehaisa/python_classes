19c44e19b5b9239715278aa4f13847af799888fe
ehaisa <isalaura1304@gmail.com>
2023-04-27 13:36:58 -0300

    estudando while

---
+++ semana7/exercicio1.py
@@ -0,0 +1,9 @@
+# na estrutura while, é obrigatóriamente false ou true
+print("Contando de 1 até 10")
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1         # a variável ficará somando até a condição se tornar falsa e o programa para
+print("Fim do programa")
+
+# toda estrutura while tem três partes: inicialização, teste e atualização
