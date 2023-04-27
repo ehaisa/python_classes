058ac7698cff921e356289d66265fe54e1f4b242
ehaisa <isalaura1304@gmail.com>
2023-04-27 13:45:35 -0300

    estudando while

---
+++ semana7/exercicio2.py
@@ -0,0 +1,11 @@
+n1 = int(input("Informe a 1a. nota: "))
+n2 = int(input("Informe a 2a. nota: "))
+n3 = int(input("Informe a 3a. nota:"))
+
+media = (n1 + n2 + n3) / 3
+print("Sua media foi: %.1f"% media)
+
+if media >= 7.0:
+    print("Parabéns! Você foi aprovado")
+else:
+    print("Pena, você foi reprovado!")
