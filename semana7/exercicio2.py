b706704dc0f4db25844035efbf4c05db18bd8bfa
ehaisa <isalaura1304@gmail.com>
2023-06-08 16:45:37 -0300

    estudando while

--- semana7/exercicio2.py
+++ semana7/exercicio2.py
@@ -1,14 +1,12 @@
-n1 = int(input("Informe a 1a. nota: "))
-n2 = int(input("Informe a 2a. nota: "))
-n3 = int(input("Informe a 3a. nota:"))
-
-media = (n1 + n2 + n3) / 3
-print("Sua media foi: %.1f"% media)
-
-if media >= 7.0:
-    print("Parabéns! Você foi aprovado")
+from random import randint
+jog = int(input("Escolha 1 p/ Cara ou 2 p/ Coroa: "))
+moeda = randint(1,2)
+if (jog == 1) and (moeda == 1):
+ print("Deu Cara, você ganhou!")
+elif (jog == 1) and (moeda == 2):
+ print("Deu Coroa, você perdeu!")
+elif (jog == 2) and (moeda == 2):
+ print("Deu Coroa, você ganhou!")
 else:
-    print("Pena, você foi reprovado!")
-
-    # ESTA VERSÃO ESTÁ ERRADA, POIS NOTAS SÃO DE VALOR FLOAT, NÃO INT
-    # ELA TAMBÉM SÓ IRÁ FUNCIONAR COM APENAS 3 NOTAS
+ print("Deu Cara, você perdeu!")
+print("Fim do Programa")
