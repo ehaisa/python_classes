4382124b2c301adf4faded3afad4b34905efa89b
ehaisa <isalaura1304@gmail.com>
2023-06-08 16:43:59 -0300

    estudando while

--- semana7/exercicio1.py
+++ semana7/exercicio1.py
@@ -1,9 +1,47 @@
-# na estrutura while, é obrigatóriamente false ou true
-print("Contando de 1 até 10")
-i = 1
-while i <= 10:
-    print(i)
-    i = i + 1         # a variável ficará somando até a condição se tornar falsa e o programa para
-print("Fim do programa")
+n1 = int(input("Informe a 1a. nota: "))
+n2 = int(input("Informe a 2a. nota: "))
+n3 = int(input("Informe a 3a. nota:"))
 
-# toda estrutura while tem três partes: inicialização, teste e atualização
+media = (n1 + n2 + n3) / 3
+print("Sua media foi: %.1f"% media)
+
+if media >= 7.0:
+    print("Parabéns! Você foi aprovado")
+else:
+    print("Pena, você foi reprovado!")
+
+    # ESTA VERSÃO ESTÁ ERRADA, POIS NOTAS SÃO DE VALOR FLOAT, NÃO INT
+    # ELA TAMBÉM SÓ IRÁ FUNCIONAR COM APENAS 3 NOTAS
+#############################################################################################################
+soma = 0
+i = 1
+while i <= 3:     # AQUI A QUANTIDADE DE NOTAS É LIMITADO PARA 3
+    nota = float(input("Informe a nota %d: "% i))
+    soma = soma + nota
+    i = i + 1
+media = soma / 3
+print("Sua media foi: %.1f"% media)
+if media >= 7.0:
+    print("Parabéns! Você foi aprovado.")
+else:
+    print("Pena, você foi reprovado.")
+
+    # ESTA VERSÃO CONTINUA FUNCIONANDO COM APENAS 3 NOTAS
+    # A PARTIR DESTE PONTO É COMEÇADO A USAR A "FUNÇÃO" CHAMADA DE ACUMULADOR
+#############################################################################################################
+soma = 0
+i = 1
+n = int(input("Informe quantas notas você quer calcular: "))
+while i <= n:
+    nota = float(input("Infome a nota %d: "%i))
+    soma = soma + nota
+    i = i + 1            # ATUALIZAÇÃO: quando o laço é finalizado, o valor de i aumenta +1 e para ao checar no valor de n
+media = soma/n
+print("Sua média foi %.1f"%media)
+if media >= 5.0:
+    print("Parabéns! Você foi aprovado!")
+else:
+    print("Pena, você foi reprovado!")
+print("Fim do Programa")
+
+# Esse programa serve para qualquer quantidade de notas
